187f155c182a42615d6002993f90234fe772b958
Robert Giacinto <robert.giacinto@gmail.com>
2012-10-10 11:05:55 +0200

    kleinere änderungen

--- app/controllers/actors/CrawlActor.scala
+++ app/controllers/actors/CrawlActor.scala
@@ -3,7 +3,7 @@ package controllers.actors
 import akka.actor.Actor
 import edu.uci.ics.crawler4j.crawler.{CrawlController, CrawlConfig, Page, WebCrawler}
 import edu.uci.ics.crawler4j.url.WebURL
-import edu.uci.ics.crawler4j.parser.HtmlParseData
+import edu.uci.ics.crawler4j.parser.{BinaryParseData, HtmlParseData}
 import edu.uci.ics.crawler4j.fetcher.PageFetcher
 import edu.uci.ics.crawler4j.robotstxt.{RobotstxtServer, RobotstxtConfig}
 
@@ -22,6 +22,9 @@ class BookmarkPageCrawler extends WebCrawler {
     page.getParseData match {
       case data: HtmlParseData =>
         indexActor ! IndexMessage(data.getText, page.getWebURL.getURL)
+      case data: BinaryParseData =>
+        println("Found binary file - " + page.getContentType)
+        //TODO Handling PDF file type - is there a way to implement a pluggable parser for crawler4j?
     }
   }
 }
